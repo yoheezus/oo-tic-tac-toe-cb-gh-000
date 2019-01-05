4c6c6ddbeb12df4469c3a2dc5c5e0867f40261d7
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-05 00:11:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,4 +15,18 @@ class TicTacToe
     def input_to_index(user_input)
         converted_input = user_input.to_i - 1
     end
+
+    def move(position, token)
+        @board[position] = token
+    end
+
+    @WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]]
 end
