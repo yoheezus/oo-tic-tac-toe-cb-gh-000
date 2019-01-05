96aa6f0b1d90e6ffbc0aee49620e9ee65a293869
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-05 00:35:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,7 @@ class TicTacToe
         end
     end
 
-    def won
+    def won?
         WIN_COMBINATIONS.detect do |combo|
             board[combo[0]] == board[combo[1]] &&
             board[combo[1]] == board[combo[2]] &&
