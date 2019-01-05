dc96824db2f6ced6d22678e36e15b2f17e6e3280
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-05 00:06:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,4 +3,8 @@ class TicTacToe
         @board = board || Array.new(9, " ")
     end
 
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
